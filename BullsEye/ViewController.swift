cfd981f058b28955b5005d10d579dead092f6b84
Luis De Castro <luisdecastro510@gmail.com>
2016-07-15 23:47:54 -0700

    add game logic and info segue

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -10,28 +10,78 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var currentValue: Int = 50
+    @IBOutlet weak var targetLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var roundLabel: UILabel!
+    @IBOutlet weak var slider: UISlider!
+    
+    var currentValue = 0
+    var targetValue = 0
+    var score = 0
+    var round = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        startNewGame()
+        updateLabels()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func startNewRound() {
+        round += 1
+        targetValue =  1 + Int(arc4random_uniform(100))
+        currentValue = 1 + Int(arc4random_uniform(100))
+        slider.value = Float(currentValue)
+    }
+    
+    func updateLabels() {
+        targetLabel.text = String(targetValue)
+        scoreLabel.text = String(score)
+        roundLabel.text = String(round)
+    }
+    
+    func startNewGame() {
+        round = 0
+        score = 0
+        startNewRound()
+    }
+    
+    @IBAction func startOver() {
+        startNewGame()
+        updateLabels()
+    }
+    
     @IBAction func showAlert() {
-        let message = "The value of the slider is \(currentValue)"
+        let difference = abs(targetValue - currentValue)
+        var points = 100 - difference
         
-        let alert = UIAlertController(title: "Hello World",
+        let message = "You scored \(points) points!"
+        var title: String
+        
+        if targetValue == currentValue {
+            title = "Bull's Eye!"
+            points += 100
+        } else if difference < 30 {
+            title = "Close!"
+        } else {
+            title = "Not even close!"
+        }
+        score += points
+        
+        let alert = UIAlertController(title: title,
                                       message: message,
                                       preferredStyle: .Alert)
         
         let action = UIAlertAction(title: "OK!",
                                    style: .Default,
-                                   handler: nil)
+                                   handler: { action in
+                                            self.startNewRound()
+                                            self.updateLabels()
+                                            })
         
         alert.addAction(action)
         
